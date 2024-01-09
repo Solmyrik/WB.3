dbff885a1c606c0953a19d2d32b25cf4c2d0b327
Dmitry <schedamian@gmail.com>
2024-01-09 22:43:10 +0300

    task3

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import "./icons";
+import Router from "./router";
+import { cartService } from "./services/cart.service";
+import { userService } from "./services/user.service";
+
+declare global {
+  interface Window {
+    userId: string;
+  }
+} 
+
+new Router();
+cartService.init();
+userService.init();
+
+setTimeout(() => {
+  document.body.classList.add("is__ready");
+}, 250);
---
+++ src/router.ts
@@ -0,0 +1,38 @@
+import { catalogComp } from './modules/catalog/catalog';
+import { notFoundComp } from './modules/notFound/notFound';
+import { homepageComp } from './modules/homepage/homepage';
+import { productDetailComp } from './modules/productDetail/productDetail';
+import { checkoutComp } from './modules/checkout/checkout';
+
+const ROUTES = {
+  '/': homepageComp,
+  '/catalog': catalogComp,
+  '/product': productDetailComp,
+  '/checkout': checkoutComp
+};
+
+export default class Router {
+  $appRoot: HTMLElement;
+
+  constructor() {
+    // @ts-ignore
+    this.$appRoot = document.querySelector('.js__root');
+    window.addEventListener('load', this.route.bind(this));
+    window.addEventListener('hashchange', this.route.bind(this));
+  }
+
+  route(e: any) {
+    e.preventDefault();
+
+    // @ts-ignore
+    const component = ROUTES[window.location.pathname] || notFoundComp;
+
+    const interval = setInterval(() => {
+      if (window.userId) {
+        component.attach(this.$appRoot);
+        component.render();
+        clearInterval(interval);
+      } 
+    }, 100);
+  }
+}
