a56524a5246d39cb09b2ca9aa4b231815320127b
Dmitry <103439106+Solmyrik@users.noreply.github.com>
2024-01-18 22:19:40 +0300

    add fetchAnalytics

--- src/services/cart.service.ts
+++ src/services/cart.service.ts
@@ -1,5 +1,6 @@
 import localforage from 'localforage';
 import { ProductData } from 'types';
+import { analytics } from './sendAnalytics.service';
 
 const DB = '__wb-cart';
 
@@ -11,6 +12,12 @@ class CartService {
   async addProduct(product: ProductData) {
     const products = await this.get();
     await this.set([...products, product]);
+
+    await analytics.fetchAnalytics({
+      type: 'addToCard',
+      payload: product,
+      timestamp: Date.now()
+    });
   }
 
   async removeProduct(product: ProductData) {
